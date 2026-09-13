c666ed0a594dc02353394b0a710720ecb96a2f3b
Víctor González <victorgovi93@gmail.com>
2026-09-14 01:53:51 +0200

    v92: bump cache version to force SW update

--- js/modules/constants.js
+++ js/modules/constants.js
@@ -1,6 +1,6 @@
 // ==================== CONSTANTES GLOBALES ====================
 
-export const CURRENT_APP_VERSION = 91;
+export const CURRENT_APP_VERSION = 92;
 
 export const UNIT_CONVERSIONS = {
     'g': 1,
