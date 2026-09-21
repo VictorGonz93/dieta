9fcf9842b1ebbe38242f9ad306c4bd81ee36c011
Víctor González <victorgovi93@gmail.com>
2026-09-21 23:20:14 +0200

    v102: bump version

--- js/modules/constants.js
+++ js/modules/constants.js
@@ -1,6 +1,6 @@
 // ==================== CONSTANTES GLOBALES ====================
 
-export const CURRENT_APP_VERSION = 101;
+export const CURRENT_APP_VERSION = 102;
 
 export const UNIT_CONVERSIONS = {
     'g': 1,
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 101; // Incrementa esto cuando hagas cambios
+const CACHE_VERSION = 102; // Incrementa esto cuando hagas cambios
 const CACHE_NAME = `nutrition-tracker-v${CACHE_VERSION}`;
 const urlsToCache = [
     '/',
