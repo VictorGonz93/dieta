f317972a0316304d45dfd6a6a2408a0ac0ad44af
Víctor González <victorgovi93@gmail.com>
2026-09-21 23:13:46 +0200

    v101: bump version

--- js/modules/constants.js
+++ js/modules/constants.js
@@ -1,6 +1,6 @@
 // ==================== CONSTANTES GLOBALES ====================
 
-export const CURRENT_APP_VERSION = 100;
+export const CURRENT_APP_VERSION = 101;
 
 export const UNIT_CONVERSIONS = {
     'g': 1,
